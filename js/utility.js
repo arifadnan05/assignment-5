3e1f0f0936839d3751f690c017d3cd89e569023c
arifadnan05 <arifahamedadnan@gmail.com>
2024-02-19 23:27:53 +0600

    javascript functional updated

--- js/utility.js
+++ js/utility.js
@@ -1,166 +1,105 @@
-const a1 = document.getElementById('A1');
-a1.addEventListener('click', function () {
-    this.classList.add('bg-[#1DD100]')
-    const seat = getValue('seat-left') - 1;
-    getId('seat-left').innerText = seat;
-    const seatCount = getValue('seat-count') + 1;
-    getId('seat-count').innerText = seatCount;
+
+document.getElementById('A1').addEventListener('click', function () {
     removeChildElementClass('a1-eco');
-    // console.log(totalPrice)
-    const totalTicketPrice = getValue('total-price');
-    const totalTicketPriceValue = totalTicketPrice + 550;
-    getId('total-price').innerText = totalTicketPriceValue;
-    grandTotal()
-    
+    const ticketPrice = getValue('total-price');
+    const totalTicketPrice = ticketPrice + 550;
+    getId('total-price').innerText = totalTicketPrice;
+    grandTotal();
+    this.classList.add("pointer-events-none");
 })
-
-const a2 = document.getElementById('A2');
-a2.addEventListener('click', function () {
-    this.classList.add('bg-[#1DD100]')
-    const seat = getValue('seat-left') - 1;
-    getId('seat-left').innerText = seat;
-    const seatCount = getValue('seat-count') + 1;
-    getId('seat-count').innerText = seatCount;
+document.getElementById('A2').addEventListener('click', function () {
     removeChildElementClass('a2-eco');
-    // console.log(totalPrice)
-    const totalTicketPrice = getValue('total-price');
-    const totalTicketPriceValue = totalTicketPrice + 550;
-    getId('total-price').innerText = totalTicketPriceValue;
-    grandTotal()
-    
+    const ticketPrice = getValue('total-price');
+    const totalTicketPrice = ticketPrice + 550;
+    getId('total-price').innerText = totalTicketPrice;
+    grandTotal();
+    this.classList.add("pointer-events-none");
 })
-
-const a3 = document.getElementById('A3');
-a3.addEventListener('click', function () {
-    this.classList.add('bg-[#1DD100]')
-    const seat = getValue('seat-left') - 1;
-    getId('seat-left').innerText = seat;
-    const seatCount = getValue('seat-count') + 1;
-    getId('seat-count').innerText = seatCount;
+document.getElementById('A3').addEventListener('click', function () {
     removeChildElementClass('a3-eco');
-    // console.log(totalPrice)
-    const totalTicketPrice = getValue('total-price');
-    const totalTicketPriceValue = totalTicketPrice + 550;
-    getId('total-price').innerText = totalTicketPriceValue;
-    grandTotal()
-    
+    const ticketPrice = getValue('total-price');
+    const totalTicketPrice = ticketPrice + 550;
+    getId('total-price').innerText = totalTicketPrice;
+    grandTotal();
+    this.classList.add("pointer-events-none");
 })
-
-const a4 = document.getElementById('A4');
-a4.addEventListener('click', function () {
-    this.classList.add('bg-[#1DD100]')
-    const seat = getValue('seat-left') - 1;
-    getId('seat-left').innerText = seat;
-    const seatCount = getValue('seat-count') + 1;
-    getId('seat-count').innerText = seatCount;
+document.getElementById('A4').addEventListener('click', function () {
     removeChildElementClass('a4-eco');
-    // console.log(totalPrice)
-    const totalTicketPrice = getValue('total-price');
-    const totalTicketPriceValue = totalTicketPrice + 550;
-    getId('total-price').innerText = totalTicketPriceValue;
-    grandTotal()
-    
+    const ticketPrice = getValue('total-price');
+    const totalTicketPrice = ticketPrice + 550;
+    getId('total-price').innerText = totalTicketPrice;
+    grandTotal();
+    this.classList.add("pointer-events-none");
 })
-
-const b1 = document.getElementById('B1');
-b1.addEventListener('click', function () {
-    this.classList.add('bg-[#1DD100]')
-    const seat = getValue('seat-left') - 1;
-    getId('seat-left').innerText = seat;
-    const seatCount = getValue('seat-count') + 1;
-    getId('seat-count').innerText = seatCount;
+document.getElementById('B1').addEventListener('click', function () {
     removeChildElementClass('b1-eco');
-    // console.log(totalPrice)
-    const totalTicketPrice = getValue('total-price');
-    const totalTicketPriceValue = totalTicketPrice + 550;
-    getId('total-price').innerText = totalTicketPriceValue;
-    grandTotal()
-    
+    const ticketPrice = getValue('total-price');
+    const totalTicketPrice = ticketPrice + 550;
+    getId('total-price').innerText = totalTicketPrice;
+    grandTotal();
+    this.classList.add("pointer-events-none");
 })
-
-const b2 = document.getElementById('B2');
-b2.addEventListener('click', function () {
-    this.classList.add('bg-[#1DD100]')
-    const seat = getValue('seat-left') - 1;
-    getId('seat-left').innerText = seat;
-    const seatCount = getValue('seat-count') + 1;
-    getId('seat-count').innerText = seatCount;
+document.getElementById('B2').addEventListener('click', function () {
     removeChildElementClass('b2-eco');
-    // console.log(totalPrice)
-    const totalTicketPrice = getValue('total-price');
-    const totalTicketPriceValue = totalTicketPrice + 550;
-    getId('total-price').innerText = totalTicketPriceValue;
-    grandTotal()
-    
+    const ticketPrice = getValue('total-price');
+    const totalTicketPrice = ticketPrice + 550;
+    getId('total-price').innerText = totalTicketPrice;
+    grandTotal();
+    this.classList.add("pointer-events-none");
 })
-
-const b3 = document.getElementById('B3');
-b3.addEventListener('click', function () {
-    this.classList.add('bg-[#1DD100]')
-    const seat = getValue('seat-left') - 1;
-    getId('seat-left').innerText = seat;
-    const seatCount = getValue('seat-count') + 1;
-    getId('seat-count').innerText = seatCount;
+document.getElementById('B3').addEventListener('click', function () {
     removeChildElementClass('b3-eco');
-    // console.log(totalPrice)
-    const totalTicketPrice = getValue('total-price');
-    const totalTicketPriceValue = totalTicketPrice + 550;
-    getId('total-price').innerText = totalTicketPriceValue;
-    grandTotal()
-    
+    const ticketPrice = getValue('total-price');
+    const totalTicketPrice = ticketPrice + 550;
+    getId('total-price').innerText = totalTicketPrice;
+    grandTotal();
+    this.classList.add("pointer-events-none");
 })
-
-const b4 = document.getElementById('B4');
-b4.addEventListener('click', function () {
-    this.classList.add('bg-[#1DD100]')
-    const seat = getValue('seat-left') - 1;
-    getId('seat-left').innerText = seat;
-    const seatCount = getValue('seat-count') + 1;
-    getId('seat-count').innerText = seatCount;
+document.getElementById('B4').addEventListener('click', function () {
     removeChildElementClass('b4-eco');
-    // console.log(totalPrice)
-    const totalTicketPrice = getValue('total-price');
-    const totalTicketPriceValue = totalTicketPrice + 550;
-    getId('total-price').innerText = totalTicketPriceValue;
-    grandTotal()
-    
+    const ticketPrice = getValue('total-price');
+    const totalTicketPrice = ticketPrice + 550;
+    getId('total-price').innerText = totalTicketPrice;
+    grandTotal();
+    this.classList.add("pointer-events-none");
 })
 
-
-function coupon () {
-
+function coupon() {
+    const seatCount = getValue('seat-count');
     const inputValue = getId('input').value;
-    if('NEW15' === inputValue || 'Couple 20' === inputValue){
-        addChildElementClass('input-field');
-        const grandTotal = getValue('grand-total');
-        
+    if (seatCount >= 4) {
+        if ('NEW15' === inputValue || 'Couple 20' === inputValue) {
+            addChildElementClass('input-field');
+            const grandTotal = getValue('grand-total');
 
-        if('NEW15'){
-            const setGrandTotal = Math.floor(grandTotal - (grandTotal * 15) / 100);
-        
-            getId('grand-total').innerText = setGrandTotal;
-        }
-        if('Couple 20'){
-            const setGrandTotal = Math.floor(grandTotal - (grandTotal * 20) / 100);
-            getId('grand-total').innerText = setGrandTotal;
+
+            if ('NEW15') {
+                const setGrandTotal = Math.floor(grandTotal - (grandTotal * 15) / 100);
+
+                getId('grand-total').innerText = setGrandTotal;
+            }
+            if ('Couple 20') {
+                const setGrandTotal = Math.floor(grandTotal - (grandTotal * 20) / 100);
+                getId('grand-total').innerText = setGrandTotal;
+            }
         }
     }
-   
+
 }
 
-function grandTotal () {
+function grandTotal() {
     const totalPrice = getValue('total-price');
     const grandTotal = getId('grand-total');
     grandTotal.innerText = totalPrice;
 }
 
-function removeChildElementClass (elementId){
+function removeChildElementClass(elementId) {
     const element = getId(elementId);
     const removeElement = element.classList.remove('hidden')
     return removeElement
 }
 
-function addChildElementClass(elementId){
+function addChildElementClass(elementId) {
     const element = getId(elementId);
     const addChildClass = element.classList.add('hidden');
     return addChildClass;
@@ -176,3 +115,32 @@ function getValue(elementId) {
     const elementTextValue = parseInt(elementText);
     return elementTextValue;
 }
+
+
+
+
+let add = 0;
+let minus = 8;
+const seatLeft = document.getElementById('seat-left');
+const seatCount = document.getElementById('seat-count');
+const seatSelection = document.getElementsByClassName('seat-class')
+for (const item of seatSelection) {
+    item.addEventListener('click', function () {
+        add++;
+        minus--;
+        if (add >= 5) {
+            // alert('Please select only 4 seat')
+            return;
+        }
+
+        seatCount.innerText = add;
+        seatLeft.innerText = minus;
+        item.style.backgroundColor = '#1DD100';
+        this.classList.add("pointer-events-none");
+
+    })
+
+
+}
+
+
